ce64adce701d471439b1a313b43bfc25338deb5e
Jordan <jordanmanu@live.com.au>
2022-04-07 15:16:41 +0100

    Added attack method and made tests pass

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Terry'
-    fill_in :player_2_name, with: 'Barry'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Terry vs. Barry'
   end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'view hitpoints' do
   scenario 'viewing player 2s hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Terry'
-    fill_in :player_2_name, with: 'Barry'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Barry: 100 hp'
   end
 end
